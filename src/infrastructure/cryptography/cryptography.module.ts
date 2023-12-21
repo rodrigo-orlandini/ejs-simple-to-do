96dd976f3514cba83ebeebf5484f373682246b7a
esoft41 <rodrigosorlandini@gmail.com>
2023-12-21 02:55:47 -0300

    partial: jwt config

--- src/infrastructure/cryptography/cryptography.module.ts
+++ src/infrastructure/cryptography/cryptography.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 
 import { Hasher } from 'src/domain/cryptography/hasher';
 import { Encrypter } from 'src/domain/cryptography/encrypter';
@@ -7,9 +8,25 @@ import { Encrypter } from 'src/domain/cryptography/encrypter';
 import { BcryptHasher } from './bcrypt-hasher';
 import { JwtEncrypter } from './jwt-encrypter';
 
+import { EnvironmentModule } from '../environment/environment.module';
+import { EnvironmentService } from '../environment/environment.service';
+
 import { AuthGuard } from './auth.guard';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [EnvironmentModule],
+      inject: [EnvironmentService],
+      global: true,
+      useFactory(environment: EnvironmentService) {
+        return {
+          signOptions: { expiresIn: '1d' },
+          secret: environment.get('JWT_SECRET'),
+        };
+      },
+    }),
+  ],
   providers: [
     { provide: Hasher, useClass: BcryptHasher },
     { provide: Encrypter, useClass: JwtEncrypter },
